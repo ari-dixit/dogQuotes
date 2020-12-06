c310850bb6593a740656c32853bb511609d597d5
ari-dixit <arihandixit@gmail.com>
2020-12-06 18:33:59 -0500

    Step 3 completed and working: ListTile, image, title and subtitle formatted

--- lib/components/dog_card.dart
+++ lib/components/dog_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dog_quotes_4/models/dog.dart';
 import 'package:dog_quotes_4/screens/details_screen.dart';
 import 'package:flutter/material.dart';
@@ -33,8 +34,27 @@ class DogCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(dog.name),
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: ListTile(
+          leading: CachedNetworkImage(
+            width: 100.0,
+            fit: BoxFit.cover,
+            imageUrl: dog.imageUrls[0],
+            placeholder: (context, imageUrl) => Center(
+              child: Container(
+                  height: 25.0,
+                  width: 25.0,
+                  child: CircularProgressIndicator()
+              ),
+            ),
+            errorWidget: (context, imageUrl, error) => Icon(Icons.broken_image),
+          ),
+              title: Text(dog.name),
+          subtitle: Text(dog.quote),
+        ),
+      ),
     );
   }
 }
